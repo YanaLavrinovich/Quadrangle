b267b20d821c15607b3d63c3746d2d84f359460a
YanaLavrinovich <Yana281298@mail.ru>
2018-10-04 20:42:41 +0300

    fix exception

--- src/main/java/by/etc/firsttask/readerFromFile/ReaderFromFileImpl.java
+++ src/main/java/by/etc/firsttask/readerFromFile/ReaderFromFileImpl.java
@@ -1,6 +1,6 @@
 package by.etc.firsttask.readerFromFile;
 
-import by.etc.firsttask.exception.ReaderIOException;
+import by.etc.firsttask.exception.ReaderFromFileException;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,11 +12,11 @@ import java.util.stream.Collectors;
 public class ReaderFromFileImpl implements ReaderFromFile {
     private static final String THE_FILE_IS_NOT_FOUND_MESSAGE = "The file is not found!";
 
-    public List<String> read(final Path path) throws ReaderIOException {
+    public List<String> read(final Path path) throws ReaderFromFileException {
         try {
             return Files.lines(path).collect(Collectors.toList());
         } catch (IOException e) {
-            throw new ReaderIOException(THE_FILE_IS_NOT_FOUND_MESSAGE, e.fillInStackTrace());
+            throw new ReaderFromFileException(THE_FILE_IS_NOT_FOUND_MESSAGE, e.fillInStackTrace());
         }
     }
 }
